34a5b1c46bced797aa4155f301a6c4cf52bbf0ee
adolan5 <andy.dolan.dev@gmail.com>
2018-11-30 22:50:06 -0700

    Stub main labeling routine

--- src/labeling/Labeler.py
+++ src/labeling/Labeler.py
@@ -4,4 +4,32 @@ class Labeler:
     formatted and had their features extracted. Provides utilities for a user
     to specify whether a message is distinct or not.
     """
-    pass
+    def __init__(self, data):
+        """Constructor.
+        Starts labeling routine of the provided data.
+        Args:
+        data (list): The extracted and formatted BGP data to be labeled.
+        """
+        # Keep internal reference to data; will be modified in place
+        self._data = data
+
+        # Initialize basic tracking
+        # This struct maps a (prefix, mask, destination) to a list of messages that were
+        # marked distinct, whether automatically (first message), or by the user
+        self._seen_distincts = {}
+
+        # Kick off labeling routine
+        print('There are {} messages to be labeled'.format(len(data)))
+        self._do_labeling()
+
+    def _do_labeling(self):
+        """Main labeling subroutine.
+        If a message composite key is encountered for the first time, that
+        message is considered distinct.
+        If a message matches a composite key already seen, ask the user if
+        it should be considered distinct, listing one or more of the most
+        recent other matches that were marked as distinct.
+        Modifies the data dict in place, setting the "distinct" field to
+        either 1 (distinct) or 0 (duplicated).
+        """
+        pass
