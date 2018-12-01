1741277ae5ba9091f335cdffc9248ae717244635
adolan5 <andy.dolan.dev@gmail.com>
2018-11-30 22:35:21 -0700

    Stub Labeler

---
+++ src/labeling/Labeler.py
@@ -0,0 +1,7 @@
+class Labeler:
+    """The Labeler class.
+    Provides the functionalities to label a set of BGP messages that have been
+    formatted and had their features extracted. Provides utilities for a user
+    to specify whether a message is distinct or not.
+    """
+    pass
