882a79efa5bc593583e9be14024969d4f25587f0
adolan5 <andy.dolan.dev@gmail.com>
2018-12-01 02:02:42 -0700

    First pass at new model of ambiguity

--- src/labeling/Labeler.py
+++ src/labeling/Labeler.py
@@ -4,6 +4,9 @@ class Labeler:
     formatted and had their features extracted. Provides utilities for a user
     to specify whether a message is distinct or not.
     """
+    # Threshold for ambiguity
+    PATH_THRESH = 0.5
+
     def __init__(self, data):
         """Constructor.
         Starts labeling routine of the provided data.
@@ -26,9 +29,8 @@ class Labeler:
         """Main labeling subroutine.
         If a message composite key is encountered for the first time, that
         message is considered distinct.
-        If a message matches a composite key already seen, ask the user if
-        it should be considered distinct, listing one or more of the most
-        recent other matches that were marked as distinct.
+        If a message matches a composite key already seen, check if the
+        "distinct-ness" is ambiguous. If so, ask the user for intervention.
         Modifies the data dict in place, setting the "distinct" field to
         either 1 (distinct) or 0 (duplicated).
         """
@@ -37,13 +39,39 @@ class Labeler:
             # Get composite key, see if it has been found before
             comp_key = tuple(mess.get('composite').values())
             if comp_key in self._seen_distincts:
-                # Prompt the user for input
-                self._prompt_user(mess, comp_key, i)
+                # Check for ambiguity, prompt user if necessary
+                self._check_ambig(mess, comp_key, i)
             # Case where the message is the first of its kind; start a new list
             else:
                 self._seen_distincts[comp_key] = [mess]
                 # Classify message as distinct
                 mess['distinct'] = 1
+
+    def _check_ambig(self, message, comp, i):
+        """Check if a conflicted message is ambiguous and requires user intervention.
+        A message is compared to the most recent distinct message that shares its
+        composite key. If the AS_PATH (full_path) elements of the messages are
+        significantly different from one another, then user intervention ensues.
+        TODO: Consider temporal difference here as well, for longer-spanning data
+        sets.
+        """
+        # Most recent distinct message with matching composite key
+        most_recent = self._seen_distincts.get(comp)[-1]
+
+        # Condition: Do the contents of the full AS_PATH of each message differ
+        # by over 50%? (no duplicates)
+        curr_path = set(message.get('full_path'))
+        recent_path = set(most_recent.get('full_path'))
+
+        diff_left = len(curr_path - recent_path) / len(curr_path.union(recent_path))
+        diff_right = len(recent_path - curr_path) / len(curr_path.union(recent_path))
+
+        # If threshold met, prompt user, let them decide
+        if diff_left > self.PATH_THRESH or diff_right > self.PATH_THRESH:
+            self._prompt_user(message, comp, i)
+        # Else, set distinct flag to duplicate (0)
+        else:
+            message['distinct'] = 0
 
     def _prompt_user(self, message, comp, i):
         """Prompt the user for if a message is unique or not.
@@ -60,7 +88,7 @@ class Labeler:
         print('\n')
         print('Message {} of {} ({:.2%})'.format(i + 1, len(self._data), ((i + 1) / len(self._data))))
         print('Time diff in sec: {}'.format(message.get('time') - most_recent.get('time')))
-        print('Composite of message exists. Most recent distinct version\'s path is:')
+        print('Message ambiguity. Most recent distinct version\'s path is:')
         print('Path: {}'.format(most_recent.get('full_path')))
         print('Message in question:')
         print('Path: {}'.format(message.get('full_path')))
