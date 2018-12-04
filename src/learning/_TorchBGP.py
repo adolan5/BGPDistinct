a70375705c43cdaa6cb153567b2e3a6b2dfa3200
adolan5 <andy.dolan.dev@gmail.com>
2018-12-04 04:46:48 -0700

    More network tweaks; high numbers on high iterations

--- src/learning/_TorchBGP.py
+++ src/learning/_TorchBGP.py
@@ -9,14 +9,17 @@ class DistinctNN(nn.Module):
     def __init__(self):
         super(DistinctNN, self).__init__()
         # Naive structure first; input > h1 > h2 > out
-        self.h1 = nn.Linear(4, 30)
+        self.h1 = nn.Linear(4, 100)
         self.a1 = nn.ReLU()
-        self.raw_out = nn.Linear(30, 1)
-        self.out_act = nn.Tanh()
+        self.h2 = nn.Linear(100, 100)
+        self.a2 = nn.ReLU()
+        self.raw_out = nn.Linear(100, 1)
+        self.out_act = nn.Sigmoid()
 
     def forward(self, x):
         out = self.h1(x)
         out = self.a1(out)
+        # out = self.h2(out)
+        # out = self.a2(out)
         out = self.raw_out(out)
-        out = self.out_act(out)
         return out
