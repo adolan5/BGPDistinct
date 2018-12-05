549ca3880d2ac6ca81f0ae65b9e030e687f95726
adolan5 <andy.dolan.dev@gmail.com>
2018-12-04 23:11:27 -0700

    Settle on a network for now

--- src/learning/_TorchBGP.py
+++ src/learning/_TorchBGP.py
@@ -13,6 +13,8 @@ class DistinctNN(nn.Module):
         self.a1 = nn.ReLU()
         self.h2 = nn.Linear(n_hidden, n_hidden)
         self.a2 = nn.ReLU()
+        self.h3 = nn.Linear(n_hidden, n_hidden)
+        self.a3 = nn.ReLU()
         self.raw_out = nn.Linear(n_hidden, 2)
         self.out_act = nn.LogSoftmax(dim=1)
 
@@ -21,6 +23,8 @@ class DistinctNN(nn.Module):
         out = self.a1(out)
         out = self.h2(out)
         out = self.a2(out)
+        out = self.h3(out)
+        out = self.a3(out)
         out = self.raw_out(out)
         out = self.out_act(out)
         return out
