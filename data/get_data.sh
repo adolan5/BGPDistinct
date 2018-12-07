d99a1889fc3605fc3885492af47bbc0958ae81d6
adolan5 <andy.dolan.dev@gmail.com>
2018-12-07 02:33:19 -0700

    Update get_data script to be path independent

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-FILE=raw_one_min_nov15.json
+# Ensure that the file is path independent
+FILE=$( dirname $0)/raw_one_min_nov15.json
+echo "Outputting file to $FILE"
 curl -o $FILE http://bgpmon.io/archive/json/routeviews4/updates?start=20181115202200\&end=20181115202300
 curl http://bgpmon.io/archive/json/routeviews2/updates?start=20181115202200\&end=20181115202300 >> $FILE
