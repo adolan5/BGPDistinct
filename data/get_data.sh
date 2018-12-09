a332441ade562f2931c5405cdfefb3b2f3a95efd
adolan5 <andy.dolan.dev@gmail.com>
2018-12-08 17:51:08 -0700

    Update get_data script to be modular

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
+
+# This script can be used to download data sets utilized for the BGPDistinct project.
+# The minute long data set is assumed when this command is run with no arguments.
+# If a single argument is passed, the large dataset is assumed (160MB).
+# Requires cURL or Wget
+
 # Ensure that the file is path independent
-FILE=$( dirname $0)/raw_one_min_nov15.json
+FILE=$(dirname $0)/raw_one_min_nov15.json
+# Assume the small file
+FIRST_URL=http://bgpmon.io/archive/json/routeviews4/updates?start=20181115202200\&end=20181115202300
+SEC_URL=http://bgpmon.io/archive/json/routeviews2/updates?start=20181115202200\&end=20181115202300
+
 # Use curl or wget
-echo "Outputting file to $FILE"
 if [ `command -v curl` ]; then
   echo "Using cURL"
-  curl -o $FILE http://bgpmon.io/archive/json/routeviews4/updates?start=20181115202200\&end=20181115202300
-  curl -o - http://bgpmon.io/archive/json/routeviews2/updates?start=20181115202200\&end=20181115202300 >> $FILE
+  COMMAND="curl -o"
 else
   echo "No cURL. Defaulting to Wget..."
-  wget -O $FILE http://bgpmon.io/archive/json/routeviews4/updates?start=20181115202200\&end=20181115202300
-  wget -O - http://bgpmon.io/archive/json/routeviews2/updates?start=20181115202200\&end=20181115202300 >> $FILE
+  COMMAND="wget -O"
 fi
+
+# If the user asked for the large file
+if [ $# -gt 0 ]; then
+  FILE=$(dirname $0)/raw_large_set_nov1.json
+  FIRST_URL=http://bgpmon.io/archive/json/routeviews2/updates?start=20181101000000\&end=20181101010000
+  SEC_URL=
+fi
+
+# Only fetch if the file does not already exist
+if [ -e $FILE ]; then
+  echo "File $FILE already exists. Aborting."
+  exit 1
+fi
+
+echo "Outputting file to $FILE"
+$COMMAND $FILE $FIRST_URL
+# Account for if there's no second url
+if [ -n "$SEC_URL" ]; then
+  $COMMAND - $SEC_URL >> $FILE
+fi
+
+exit 0
